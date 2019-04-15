68ad1b8663650484e3a2285f7c5b8badc4066ad3
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2019-04-15 10:02:21 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,34 +1,26 @@
 options(warn=-1)
 
+if (!require("shiny"))
+    install.packages("shiny")  
+
+if (!require("shinyFiles"))
+    install.packages('shinyFiles')
+
+if (!require("xtable"))
+    install.packages("xtable")
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+install.packages("BiocManager")
+
+#if (!require("flowAI"))
+#BiocManager::install("flowAI", version = "3.8")
+
+if (!require("openCyto"))
+BiocManager::install("openCyto")
+
 library(shiny)
 library(openCyto)
 library(xtable)
-
-#shinyUI(
-#  fluidPage(
-#    sidebarLayout(
-#      sidebarPanel(
-#        titlePanel("Please upload your *.fcs files:"),
-#        fileInput(inputId = "Files", label = "Select Samples", multiple = TRUE, accept = ".fcs"),
-#        titlePanel("Please choose your output files:"),
-#        checkboxInput("output_hQC", "Write file with high quality events only (\"_hQC.fcs\")", value = TRUE),
-#        checkboxInput("output_lQC", "Write file with low quality events only (\"_lQC.fcs\")", value = TRUE),
-#        checkboxInput("output_QC", "Write file with both low (remove_from_all/QC < 10,000) and high (remove_from_all/QC > 10,000) quality events (\"_QC.fcs\")", value = TRUE)
-#        #shinyDirButton("output_dir", "Choose directory", "Upload")
-#        ),
-
-#      mainPanel(
-#        titlePanel("The results will be print here:"),
-#        textOutput("inputFiles"),
-#        textOutput("Samples")
-#        #verbatimTextOutput("dir"), br()
-#        #tableOutput("flowAI")
-#        #tableOutput("exprs")
-#      )
-#    )
-#  )
-#)
-
 
 ui <- fluidPage(
   br(),
