1fd4f1dbe8c9b3906ef284cb2ab88a39e4e38a1b
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2019-08-28 16:22:19 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -12,9 +12,6 @@ if (!require("xtable"))
 if (!requireNamespace("BiocManager", quietly = TRUE))
 install.packages("BiocManager")
 
-if (!require("flowAI"))
-BiocManager::install("flowAI")
-
 if (!require("openCyto"))
 BiocManager::install("openCyto")
 
@@ -22,8 +19,6 @@ library(shiny)
 library(shinyFiles)
 library(openCyto)
 library(xtable)
-library(flowAI)
-source("flowAIallshinyfunctions.R")
 print(sessionInfo())
 
 ui <- fluidPage(
@@ -31,7 +26,7 @@ ui <- fluidPage(
   tags$head(
     tags$style(".title {margin: auto; width: 800px; font-size-adjust: 1.3}")
   ),
-  tags$div(class="title", titlePanel("Shiny FlowAI")),
+  tags$div(class="title", titlePanel("Shiny OpenCyto")),
 br(),
   headerPanel(
     list(HTML('<img src="Logo_cptp.png"/>'), HTML('<a href="https://www.cptp.inserm.fr/en/technical-platforms/genomic-and-transcriptomic/">Genomic and transcriptomic platform</a>'), HTML('<img src="Logo_inserm.png"/>')),
@@ -46,9 +41,8 @@ br(),
       h3("Please upload your *.fcs files:"),
       fileInput(inputId = "Files", label = "Select Samples", multiple = TRUE, accept = ".fcs"),
       h3("Please choose your output options:"),
-      checkboxInput("output_hQC", "Write file with high quality events only (\"_hQC.fcs\")", value = TRUE),
-      checkboxInput("output_lQC", "Write file with low quality events only (\"_lQC.fcs\")", value = FALSE),
-      checkboxInput("output_QC", "Write file with both low (remove_from_all/QC < 10,000) and high (remove_from_all/QC > 10,000) quality events (\"_QC.fcs\")", value = TRUE)    ),
+      fileInput(inputId = "gatingstrategy", label = "Gating strategy", multiple = FALSE, accept = c("text/csv", "text/comma-separated-values, text/plain", ".csv"))
+  ),
     
     mainPanel(
       h4("flowAI is able to perform an automatic or interactive quality control on FCS data acquired using flow cytometry instruments. By evaluating three different properties: 1) flow rate, 2) signal acquisition, 3) dynamic range, the quality control enables the detection and removal of anomalies."),
